9dd000a5c37373cb20ed7bd46b998af1fc80c958
joshschoenberg <jschoenberg@princeton.edu>
2024-03-27 21:53:14 -0400

    create_database creation
    Working versions (with try/except) of create_database files

--- create_student_database.py
+++ create_student_database.py
@@ -1,20 +1,14 @@
 # Consulted with ChatGPT: 
 import os
+import sys
 from pymongo import MongoClient
 
-# Connect to MongoDB
-try:
-    client = MongoClient('mongodb+srv://jschoenberg:TigerPlan123!@tigerplantest1.lmveo2u.mongodb.net/?retryWrites=true&w=majority&appName=TigerPlanTest1')
-    db = client['TigerPlanTest1']
-except Exception as ex:
-    print("Connection unsuccessful")
+try: 
+    with MongoClient('mongodb+srv://tigerplan333:TigerPlan123!@tigerplandata.yyrhywn.mongodb.net/') as client:
+        db = client['TigerPlanData']
+        db.create_collection('StudentsData')
     
-#s Transform and insert data into MongoDB
-# db.create_collection('TestDB1AFS')
-db.create_collection('StudentsData')
+except Exception as ex: 
+    print(ex, file=sys.stderr)
+    sys.exit(1)
 
-#In this example, replace 'data.yaml' with the path to your YAML file, 
-# 'mongodb://localhost:27017/' with the connection string for your 
-# MongoDB instance, 'mydatabase' with the name of your MongoDB database, 
-#and 'mycollection' with the name of the collection where you want to 
-# insert the data.
