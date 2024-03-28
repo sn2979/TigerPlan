f516a83e7ad638eee923e7e25c91bf3680cbb684
sn2979 <sn2979@princeton.edu>
2024-03-28 03:19:37 -0400

    Created Student DB and student DB retriever
    The two programs I created do the following:
    
    create_student_database creates the collection of students and assigns the indices associated with students: netid, name, major, classes, and recommendations
    
    student_database.py: This is what we would want to use to display class information on the class board! Basically, if a student doesn't exist in the database, the student gets added to the database through this program. If they are already on the database, then you proceed as usual. In addition, you can get the classes of a given student by passing in their username into a get_student_classes method. You can update the classes of a student similarly by passing in the classes to be updated and the student's username as well.

--- create_student_database.py
+++ create_student_database.py
@@ -6,9 +6,14 @@ from pymongo import MongoClient
 try: 
     with MongoClient('mongodb+srv://tigerplan333:TigerPlan123!@tigerplandata.yyrhywn.mongodb.net/') as client:
         db = client['TigerPlanData']
-        db.create_collection('StudentsData')
-    
+        collection = db['StudentsData']
+        print("Connected to MongoDB successfully!")
+        collection.create_index("netID", unique=True)
+        collection.create_index("Name")
+        collection.create_index("Major")
+        collection.create_index("Classes")
+        collection.create_index("Recommendations")
+        print("Created indices successfully!")
 except Exception as ex: 
     print(ex, file=sys.stderr)
     sys.exit(1)
-
---
+++ student_database.py
@@ -0,0 +1,84 @@
+import sys
+from pymongo import MongoClient
+
+try:
+    client = MongoClient('mongodb+srv://tigerplan333:TigerPlan123!@tigerplandata.yyrhywn.mongodb.net/?retryWrites=true&w=majority&appName=TigerPlanData')
+    db = client['TigerPlanData']
+    students_collection = db['StudentsData']
+    print("Connected to MongoDB successfully!")
+except Exception as e:
+    print("Failed to connect to MongoDB:", e, file=sys.stderr)
+    sys.exit(1)
+
+# Function to handle student login
+def handle_student_login(username):
+    try:
+        # Check if student exists in the database
+                
+        existing_student = students_collection.find_one({"netID": username})
+        #print(existing_student)
+        #print("find one worked!")
+
+        if existing_student is None:
+            #print("Student doesn't exist!")
+             # If student doesn't exist, create a new student document
+            new_student = {
+                "netID": username,
+                "Name": "",
+                "Major": "",
+                "Classes": [],
+                "Recommendations": []
+            }   
+            students_collection.insert_one(new_student)
+            #print("insert one worked!")
+        return True
+    except Exception as e:
+        print("An error occurred while handling student login:", e)
+        return False
+
+# Function to get student classes
+def get_student_classes(username):
+    try:
+        existing_student = students_collection.find_one({"netID": username})
+        if existing_student is None:
+            raise Exception("Student not found")
+        return existing_student.get("Classes", [])
+    except Exception as e:
+        print("An error occurred while getting student classes:", e)
+        return []
+    
+# Function to update student classes
+def update_student_classes(username, classes):
+    try:
+        existing_student = students_collection.find_one({"netID": username})
+        if existing_student is None:
+            raise Exception("Student not found")
+        students_collection.update_one({"netID": username}, {"$set": {"Classes": classes}})
+        return True
+    except Exception as e:
+        print("An error occurred while updating student classes:", e)
+        return False
+
+# Example usage
+def main():
+    try:
+        # Assuming username is retrieved after successful login
+        # create a loop of usernames
+        for i in range(1, 10):
+            username = "student" + str(i)
+            worked = handle_student_login(username)
+            if worked:
+                print("Student login successful!")
+                student_classes = get_student_classes(username)
+                print("Classes for student:", student_classes)
+            else:
+                print("Student login failed!")
+        # Update student classes
+        update_student_classes("student2", ["COS 333", "COS 226", "COS 217"])
+        student_classes = get_student_classes("student2")
+        print("Updated classes for student:", student_classes)
+    except Exception as e:
+        print("An error occurred:", e)
+
+if __name__ == "__main__":
+    main()
