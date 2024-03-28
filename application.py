bf080aec1b8971be499b330589c20a8d4694daa8
tarashukla <ts6796@princeton.edu>
2024-03-27 23:26:47 -0400

    img dupl delete, folder refactor, html debug

--- application.py
+++ application.py
@@ -2,14 +2,15 @@
 import flask
 import auth
 
-app = flask.Flask(__name__,  template_folder='.')
+app = flask.Flask(__name__,  template_folder='templates')
 app.secret_key = '12345'
 #-----------------------------------------------------------------------
 @app.route('/', methods=['GET'])
 @app.route('/index', methods=['GET'])
 def index():
     username = auth.authenticate()
-    html_code = flask.render_template("/HomePage HTML/css/abani's_attempt.html", username=username)
+    html_code = flask.render_template("abani's_attempt.html", username=username)
+
     response = flask.make_response(html_code)
     return response
 
