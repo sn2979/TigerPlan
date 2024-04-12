03c5b27623eb84cf5e8f3549d708e97863a1a638
abani10 <abani.ahmed@princeton.edu>
2024-04-12 16:04:57 -0400

    removed hard coded netid

--- auth.py
+++ auth.py
@@ -52,7 +52,7 @@ def validate(ticket):
 # Do not return unless the user is successfully authenticated.
 
 def authenticate():
-    return "aa3293"
+    
     # If the username is in the session, then the user was
     # authenticated previously.  So return the username.
     if 'username' in flask.session:
