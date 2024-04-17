f4697f2ea8fc08efbece9770e9bef5a953979069
tarashukla <ts6796@princeton.edu>
2024-04-16 23:29:24 -0400

    more design edits

--- application.py
+++ application.py
@@ -325,6 +325,7 @@ def recommendations():
     for course in stored_recommendations:
         course['minorid'] = course['minor']
         course['minor'] = map_major_id_to_name(course['minor'])
+        course['desc'] = "MAWHAHAHAHHAHA"
 
     return flask.render_template("recommend.html", username=username, courses=stored_recommendations)
 
