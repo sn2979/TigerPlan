e7df6030ad8ff7490e93b870ebd237c8d273bd5f
tarashukla <ts6796@princeton.edu>
2024-04-18 20:22:51 -0400

    made minors database

---
+++ database_files/minors_database.py
@@ -0,0 +1,67 @@
+import sys
+import os
+import queue
+from pymongo import MongoClient, DESCENDING
+
+#-----------------------------------------------------------------------
+# _DATABASE_URL = os.environ['DATABASE_URL'] # = mongodb+srv://tigerplan333:TigerPlan123!@tigerplandata.yyrhywn.mongodb.net/?retryWrites=true&w=majority&appName=TigerPlanData
+_connection_pool = queue.Queue()
+
+def _get_connection():
+    try:
+        conn = _connection_pool.get(block=False)
+    except queue.Empty:
+        # conn = MongoClient(_DATABASE_URL)
+        conn = MongoClient("mongodb+srv://tigerplan333:TigerPlan123!@tigerplandata.yyrhywn.mongodb.net/?retryWrites=true&w=majority&appName=TigerPlanData")
+    return conn
+
+def _put_connection(conn):
+    _connection_pool.put(conn)
+
+#-----------------------------------------------------------------------
+try:
+    client = _get_connection()
+    db = client['TigerPlanData']
+    minors_collection = db['MinorsData']
+    print("Connected to MongoDB successfully!")
+except Exception as e:
+    print("Failed to connect to MongoDB:", e, file=sys.stderr)
+    sys.exit(1)
+finally:
+    _put_connection(client)
+
+# Function to get minor description
+def get_desc(minorId):
+    try:
+        minor = minors_collection.find_one({"code": minorId})
+        if minor is None:
+            raise Exception("Minor not found")
+        return minor.get("description", "")
+    except Exception as e:
+        print("An error occurred while getting minor description:", e)
+        return ""
+
+#Function to get minor URLs
+def get_urls(minorId):
+    try:
+        minor = minors_collection.find_one({"code": minorId})
+        if minor is None:
+            raise Exception("Minor not found")
+        return minor.get("urls", [])
+    except Exception as e:
+        print("An error occurred while getting minor description:", e)
+        return []
+
+
+# Example usage
+def main():
+    try:
+        minorID = "COS"
+        desc = get_desc(minorID)
+        print(desc)
+        print("hoho")
+    except Exception as e:
+        print("An error occurred:", e)
+
+if __name__ == "__main__":
+    main()
